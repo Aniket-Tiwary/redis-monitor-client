ef60f26291b853ec5d97ba3f17018e7c3242fce2
Aniket Tiwary <17uec020@lnmiit.ac.in>
2020-11-08 20:52:27 +0530

    feat: add heroku api

--- src/apis/monitor-api.js
+++ src/apis/monitor-api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  // baseURL: 'https://sheetcode-api.herokuapp.com/'
-  baseURL: "http://localhost:8080/",
+  baseURL: "https://redis-monitor-api.herokuapp.com/",
+  // baseURL: "http://localhost:8080/",
 });
